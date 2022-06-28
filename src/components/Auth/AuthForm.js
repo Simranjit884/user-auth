44ae0ff500ff3d024f906d5dbeaed2bab240ad80
samy884 <ssgill884@gmail.com>
2022-06-28 20:46:44 +0530

    post request for signup

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useRef, useState } from "react";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -12,36 +14,47 @@ const AuthForm = () => {
   const onSubmitHandler = (event) => {
     event.preventDefault();
 
-    fetch('', {
-      method: 'POST',
+    fetch("AIzaSyCTpLpC1-TQb_RLvQhJWd1Uk5CX5mCsRnA", {
+      method: "POST",
       body: JSON.stringify({
+        email: emailInputRef,
+        password: passwordInputRef,
+        returnSecureToken: true,
       }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      if (res.ok) {
+        console.log(res);
+      } else {
+        res.json().then((error) => {
+          console.log(error);
+        });
       }
     });
-  }
+  };
 
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
       <form onSubmit={onSubmitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" required />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
