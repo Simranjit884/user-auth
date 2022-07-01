d0a715d8c7a06f5bdffcf08bad23bb7f949019cb
samy884 <ssgill884@gmail.com>
2022-07-01 19:42:50 +0530

    created authContext

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AuthContext = React.createContext({
-    token: '',
-    isLoggedIn: false,
-    login: () => {},
-    logout: () => {}
+  token: "",
+  isLoggedIn: false,
+  login: () => {},
+  logout: () => {},
 });
+
+export const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
+
+  const userLoggedIn = !!token;
+
+  const loginHandler = (token) => {
+    setToken(token);
+  };
+
+  const logoutHandler = () => {
+    setToken(null);
+  };
+
+  const authValue = {
+    token: token,
+    isLoggedIn: userLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={authValue}>
+      {props.childern}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
