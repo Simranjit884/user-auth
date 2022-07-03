0c3fc7be53366063a8e0517d75ca1b00d941cefa
samy884 <ssgill884@gmail.com>
2022-07-03 17:15:24 +0530

    fetch password

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,37 @@
-import classes from './ProfileForm.module.css';
+import { useRef } from "react";
+import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPasswordd = newPasswordInputRef.input.value;
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCTpLpC1-TQb_RLvQhJWd1Uk5CX5mCsRnA",
+      {
+        method: "POST",
+        body: JSON.stringify(),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={onSubmitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
