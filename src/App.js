1309b0f8719a132e02c8ec1877553ef20de9366e
samy884 <ssgill884@gmail.com>
2022-07-03 18:12:07 +0530

    conditional routes rendering

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import { Switch, Route } from 'react-router-dom';
-
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import { Switch, Route, Redirect } from "react-router-dom";
+import AuthContext from "./store/auth-context";
+import { useContext } from "react";
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
+  const authCtxt = useContext(AuthContext);
+
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
+        <Route path="/" exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
+        {!authCtxt.isLoggedIn && (
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+        )}
+          <Route path="/profile">
+            {authCtxt.isLoggedIn && <UserProfile />}
+            {!authCtxt.isLoggedIn && <Redirect to='/auth' />}
+          </Route>
+        <Route path="*">
+          <Redirect to="/" />
         </Route>
       </Switch>
     </Layout>
